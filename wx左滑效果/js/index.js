816b8ce8039c112b25562f80596a23fd5fc083ec
Razzh <136013779@qq.com>
2021-10-25 11:11:43 +0800

    add:滑块删除

--- wx左滑效果/js/index.js
+++ wx左滑效果/js/index.js
@@ -1,12 +1,13 @@
 let oList = document.getElementsByClassName('list')[0],
     oListItem = document.getElementsByClassName('list-item'),
     startX,
-    moveX;
+    moveX,
+    oDel, // 删除按钮
+    index;
     oList.addEventListener('touchstart', handleTouchStart)
     oList.addEventListener('touchmove', handleTouchMove)
     oList.addEventListener('touchend', handleTouchEnd)
     oList.addEventListener('click', handleClick)
-
     function handleTouchStart(e) {
         startX = e.touches[0].pageX; // 记录开始的X坐标
     }
@@ -15,25 +16,35 @@ let oList = document.getElementsByClassName('list')[0],
         moveX = e.touches[0].pageX; // 记录移动时候的X坐标
         let event = e.target || window.event;
         let item = event.closest('.list-item');
-        if(startX - moveX >= 50) {
-            item.classList.remove('silde-away');
-            item.classList.add('animation');
-            item.classList.add('silde-out');
+        oDel = event.closest('.cheat').getElementsByClassName('del')[0];
+        index = item.getAttribute('data-index');
+        if(startX - moveX >= 50) { // 左滑动距离大于删除模块的宽度时
+            item.classList.remove('silde-away'); // 第一次滑块后，第二次使用时清除样式
+            item.classList.add('animation'); // 向list-item添加过渡效果
+            item.classList.add('silde-out'); // 将list-item的transfrom:translate 0 -> -50px
         }
-        if(moveX - startX >= 50) {
-            item.classList.remove('silde-out');
-            item.classList.add('silde-away');
+        if(moveX - startX >= 50) { // 右滑动触发
+            item.classList.remove('silde-out'); // 清除左滑时候的样式
+            item.classList.add('silde-away'); // 添加复位样式:list-item的transfrom:translate -50 -> 0px
         }
     }
 
-    function handleTouchEnd(e) {
-        console.log('end')
+    function handleTouchEnd() {
+        oDel.addEventListener('click', handleDel) // 删除操作
     }
     
     function handleClick(e) { // 点击之后聊天框归位
         let event = e.target || window.event,
             item = event.closest('.list-item');
-
-        item.classList.remove('silde-out');
-        item.classList.add('silde-away');
+        if(item) { // 点击到list-item才能复位
+            item.classList.remove('silde-out');
+            item.classList.add('silde-away');
+        }
     }
+    function handleDel(e) {
+        console.log(e.target)
+        let event = e.target || window.event,
+            item = event.closest('.cheat');
+        item.remove();
+    }
+    
