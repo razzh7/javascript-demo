d7521cd8fd3c816b83a1cb129cf67b8612d1f72f
Razzh <136013779@qq.com>
2021-10-25 09:31:25 +0800

    add:基本滑块效果

--- wx左滑效果/js/index.js
+++ wx左滑效果/js/index.js
@@ -2,25 +2,38 @@ let oList = document.getElementsByClassName('list')[0],
     oListItem = document.getElementsByClassName('list-item'),
     startX,
     moveX;
-
     oList.addEventListener('touchstart', handleTouchStart)
     oList.addEventListener('touchmove', handleTouchMove)
     oList.addEventListener('touchend', handleTouchEnd)
+    oList.addEventListener('click', handleClick)
 
     function handleTouchStart(e) {
         startX = e.touches[0].pageX; // 记录开始的X坐标
-        console.log('start',startX);
     }
 
     function handleTouchMove(e) {
         moveX = e.touches[0].pageX; // 记录移动时候的X坐标
         let event = e.target || window.event;
-        let oDel = event.getElementsByClassName('del')[0];
-        if(startX - moveX > 50) {
-            oDel.classList.add('active');
+        let item = event.closest('.list-item');
+        if(startX - moveX >= 50) {
+            item.classList.remove('silde-away');
+            item.classList.add('animation');
+            item.classList.add('silde-out');
+        }
+        if(moveX - startX >= 50) {
+            item.classList.remove('silde-out');
+            item.classList.add('silde-away');
         }
     }
 
     function handleTouchEnd(e) {
         console.log('end')
+    }
+    
+    function handleClick(e) { // 点击之后聊天框归位
+        let event = e.target || window.event,
+            item = event.closest('.list-item');
+
+        item.classList.remove('silde-out');
+        item.classList.add('silde-away');
     }
